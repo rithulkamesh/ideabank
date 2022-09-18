2408f2655967cdd393d02917647a23c627890000
Rithul Kamesh <hi@rithul.dev>
2022-09-18 16:51:46 +0530

    Added warning while creating note with already existing name

--- src/ideas.rs
+++ src/ideas.rs
@@ -1,10 +1,32 @@
 use crate::dir::get_idea_from_title;
 use chrono::Utc;
-use std::{env::var, fs, io::Write, process::Command};
+use std::{
+    env::var,
+    fs,
+    io::{stdin, Write},
+    path::PathBuf,
+    process::Command,
+};
 
 pub fn new_idea(title: &str) {
     let dir = get_idea_from_title(title);
     let created_date = Utc::now().timestamp();
+    if dir.exists() {
+        println!("There already exists a idea with this name. Proceed? (y/n)");
+        let mut buf = String::from("");
+        stdin().read_line(&mut buf).unwrap();
+        return match &buf[..] {
+            "y\n" => {
+                create_idea(dir.clone(), title, created_date);
+            }
+            _ => return println!("\nAborting..."),
+        };
+    } else {
+        create_idea(dir.clone(), title, created_date);
+    }
+}
+
+fn create_idea(dir: PathBuf, title: &str, created_date: i64) {
     let mut file = fs::File::create(dir.clone()).unwrap();
     writeln!(
         &mut file,
