8dbec84f79c06ba48566d38e78dcb76997f1a045
Rithul Kamesh <hi@rithul.dev>
2022-09-20 19:36:56 +0530

    Removed annoying fleet doing bs

--- src/target/debug/deps/src-50210909c4770cdc.d
+++
@@ -1,8 +0,0 @@
-/Users/rithulk/dev/ideabank/src/target/debug/deps/src-50210909c4770cdc: main.rs cli.rs dir.rs ideas.rs
-
-/Users/rithulk/dev/ideabank/src/target/debug/deps/src-50210909c4770cdc.d: main.rs cli.rs dir.rs ideas.rs
-
-main.rs:
-cli.rs:
-dir.rs:
-ideas.rs:
--- src/target/debug/deps/src-98e84a4e5dd10006.d
+++
@@ -1,8 +0,0 @@
-/Users/rithulk/dev/ideabank/src/target/debug/deps/src-98e84a4e5dd10006.rmeta: main.rs cli.rs dir.rs ideas.rs
-
-/Users/rithulk/dev/ideabank/src/target/debug/deps/src-98e84a4e5dd10006.d: main.rs cli.rs dir.rs ideas.rs
-
-main.rs:
-cli.rs:
-dir.rs:
-ideas.rs:
